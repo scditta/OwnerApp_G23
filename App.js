5bd00fb1f4a570359566e2b7b816aae649788b9b
Stephen <sdittawork@gmail.com>
2024-04-10 23:51:52 -0400

    Implemented firebase connection and testing

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import { db } from './firebaseConfig';
+import { collection, addDoc } from 'firebase/firestore';
 
 export default function App() {
+
+  const buttonPressed = async () => {
+    // insert into database
+    try {
+      // this code inserts into the "students" collecction
+      // addDoc() will return you a copy of the document that was inserted
+      const docRef = await addDoc(collection(db, "test"), {
+        firstname: "Mark",
+        lastname: "Grinch",
+        num: 2.5
+      })
+      alert("Data inserted, check console for output")
+      console.log(`Id of inserted document is: ${docRef.id}`)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button onPress={buttonPressed} title='Insert to database'></Button>
     </View>
   );
 }
